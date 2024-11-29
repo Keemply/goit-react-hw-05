498e9b31b1cbf48eef6edeba43be199bc5931d67
Keemply <144055348+Keemply@users.noreply.github.com>
2024-11-29 15:58:12 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import "./App.css";
-import { Route, Routes, useLocation } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { lazy, Suspense } from "react";
-import { useEffect, useState } from "react";
-import { getTrendMovies } from "./tmdbApi";
+
 import NotFoundPage from "./pages/NotFoundPage";
 import Navigation from "./components/Navigation/Navigation";
 const HomePage = lazy(() => import("./pages/HomePage"));
@@ -14,29 +13,12 @@ const MovieReviews = lazy(() =>
 );
 
 function App() {
-  const [trendMovies, setTrendMovies] = useState(null);
-  const location = useLocation();
-
-  useEffect(() => {
-    if (location.pathname !== "/") {
-      return;
-    }
-    async function wrapper() {
-      const movies = await getTrendMovies();
-      setTrendMovies(movies);
-    }
-    wrapper();
-  }, [location]);
-
   return (
     <div>
       <Navigation />
       <Suspense fallback={<div>Loading page...</div>}>
         <Routes>
-          <Route
-            path="/"
-            element={<HomePage trendMovies={trendMovies} location={location} />}
-          />
+          <Route path="/" element={<HomePage />} />
           <Route path="/movies" element={<MoviesPage />} />
           <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
             <Route path="cast" element={<MovieCast />} />
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,22 @@
+import { useEffect, useState } from "react";
 import MovieList from "../components/MovieList/MovieList";
+import { useLocation } from "react-router-dom";
+import { getTrendMovies } from "../tmdbApi";
 
-function HomePage({ trendMovies, location }) {
+function HomePage() {
+  const [trendMovies, setTrendMovies] = useState(null);
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname !== "/") {
+      return;
+    }
+    async function wrapper() {
+      const movies = await getTrendMovies();
+      setTrendMovies(movies);
+    }
+    wrapper();
+  }, [location]);
   return (
     <div>
       <h1>Trending today</h1>
